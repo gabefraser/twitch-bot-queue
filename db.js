bec8053f6b82dc7269147865c3f95e5ab1f563c2
gabefraser <gabriel.fraser223@gmail.com>
2018-10-08 20:34:17 +1100

    Addition: Basic INSERT SQL

--- db.js
+++ db.js
@@ -13,3 +13,17 @@ con.connect(function(err){
     if(err) throw err;
     console.log(`Connected to ${db.host}`);
 });
+
+let insert = function(query){
+    // Create the start of the query.
+    const command = "INSERT INTO";
+    const sql = `${command} ${query}`;
+
+    // Execute the INSERT.
+    con.query(sql, function (err, result) {
+        if(err) throw err;
+        console.log(`Executed the ${query} with a result of `);
+    });
+}
+
+module.exports.insert = insert;
